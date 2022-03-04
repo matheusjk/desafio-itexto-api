aa29ace3c6ed7e0f5aaa1ae193fbb6d3ff49b49a
Matheus Rodrigues <41797759+matheusjk@users.noreply.github.com>
2022-03-04 20:44:05 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ class Config(object):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://testolino:testolirico@devall.com.br:3306/teste"
+    SQLALCHEMY_DATABASE_URI = ""
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_ECHO = False
     SECRET_KEY = 'pythonApi'
 
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://testolino:testolirico@devall.com.br:3306/teste"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://:3306"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_ECHO = False
     SECRET_KEY = 'pythonApi'
