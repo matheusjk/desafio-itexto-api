f0162d365ffea1fb36f3f2844cde22ce2542da00
Matheus Rodrigues <matheusrodriguesh1@gmail.com>
2022-03-06 22:36:25 -0300

    executa o projeto

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import create_app
 if __name__ == '__main__':
     app = create_app()
     app.app_context().push()
-    app.run(host='192.168.0.14', port=59000)
+    app.run(host='192.168.0.14', port=5000)
